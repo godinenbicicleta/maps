dbe9baf58d7e8d40c198a342327475c82a8580f9
Bruno Carballo <carballo.bruno@gmail.com>
2019-01-17 23:07:13 -0600

    update to leaflet 1.4, voronoi now works

--- mex01/code.js
+++ mex01/code.js
@@ -2,33 +2,56 @@
 //const delaunay = d3.Delaunay.from(points);
 //const voronoi = delaunay.voronoi([0, 0, 960, 500]);
 
-//create a new map object with a carto tile
+
+
 var map = new L.Map("map").setView([23.1,-100.1],5)
-    .addLayer(new L.TileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
-{attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
- maxZoom:18}
+  .addLayer(new L.TileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+      {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    maxZoom:18}
 		));
+L.svg({clickable:true}).addTo(map);
 
-//overlay an svg to the map
-var svg = d3.select(map.getPanes().overlayPane).append("svg").attr("class","map-svg"),
-    g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-d3.json("mxstates-topo-simplify.json").then( (states) => {
-  //
+var svg = d3.select("#map").select("svg").attr("pointer-events", "auto")
+  g = svg.append("g"),
+  g2 = svg.append("g");
 
-  var geoData = topojson.feature(states,states.objects.mxstates), //states polygons
-    displayW = document.getElementById("vis-container").clientWidth,
+var transform = d3.geoTransform({point: projectPoint}),
+  path = d3.geoPath().projection(transform);
+
+
+d3.csv("edos_ptos.csv").then(voros);
+d3.json("mxstates-topo-simplify.json").then(main);
+
+function main(states){
+  var data = topojson.feature(states,states.objects.mxstates);
+  console.log(data);
+  drawFeatures(data);
+  }
+
+
+function projectPoint(x, y) {
+    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+    this.stream.point(point.x, point.y);
+}
+
+function drawFeatures(data){
+  
+  var displayW = document.getElementById("vis-container").clientWidth,
     displayH = document.getElementById("vis-container").clientHeight;
 
- // to build a path first project using projectPoint
-  var transform = d3.geoTransform({point: projectPoint}),
-      path = d3.geoPath().projection(transform);
+	var driv = d3.select("#vis-container")
+    .append("div")
+    .attr("class","tooltip")
+    .style("opacity", 0);
 
-
-	var driv = d3.select("#vis-container").append("div").attr("class","tooltip").style("opacity", 0);
-	var tooltip = d3.select("body").append("div").attr("class","tooltip");
+	var tooltip = d3.select("body")
+    .append("div")
+    .attr("class","tooltip");
+  
   //Position of the tooltip relative to the cursor
   var tooltipOffset = {x: 5, y: -25};
+  
   //Move the tooltip to track the mouse
   function moveTooltip() {
       tooltip.style("top",(d3.event.pageY+tooltipOffset.y)+"px")
@@ -50,54 +73,77 @@ d3.json("mxstates-topo-simplify.json").then( (states) => {
 		  .style("display","flex");
   }
 
-  var feature = g.selectAll("path")
-      .data(geoData.features)
-      .enter().append("path")
-      .on("click",showTooltip)
-      .on("mousemove",moveTooltip)
-      .on("mouseout",hideTooltip);
+  var featureElement = g.selectAll("path")
+    .data(data.features)
+    .enter()
+    .append("path")
+    .attr("class","states")
+    .on("click",showTooltip)
+    .on("mousemove",moveTooltip)
+    .on("mouseout",hideTooltip);
 
-			
-			
+    map.on("moveend", update);
 
-  map.on("viewreset", reset);
-  reset();
-  
-  d3.select("#bikes").on("change", reset);
+    update();
 
+    function update(){
+      //update polygons
+      featureElement.attr("d", path).style("fill-opacity", 0.7);
 
-  // Reposition the SVG to cover the features.
-  function reset() {
-		var bounds = path.bounds(geoData);
-    var topLeft = bounds[0],
-        bottomRight = bounds[1];
-
-    svg.attr("width", bottomRight[0] - topLeft[0])
-        .attr("height", bottomRight[1] - topLeft[1])
-        .style("left", topLeft[0] + "px")
-        .style("top", topLeft[1] + "px");
-
-    g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-
-    feature.attr("d", path)
-		    .style("fill-opacity", 0.7);
-
-    
-    if(d3.select("#bikes").property("checked")){
-      feature.attr("d", path)
-        .style("fill-opacity", 1);
-      
       }
-
 }
 
 
+function voros(data){
+  geoData = toGeoJson(data);
 
-  // Use Leaflet to implement a D3 geometric transformation.
-  function projectPoint(x, y) {
-    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-    this.stream.point(point.x, point.y);
+  var featureElement = g2.selectAll("path")
+    .data(geoData.features)
+    .enter()
+    .append("path")
+    .attr("class","voronoi")
+
+    map.on("moveend", update);
+
+    update();
+    
+    d3.select("#bikes").on("change", update);
+
+    function update(){
+      //update polygons
+      featureElement.attr("d", path).style("fill-opacity", 0.7).style("stroke","none");
+      
+      //if box checked, change polygons style
+      if(d3.select("#bikes").property("checked")){
+        featureElement.style("stroke","black" );
+
+        }
+    }
+
+
+}
+
+function toGeoJson(data){
+  let geoms = [];
+  let points = [];
+  for(let p of data){
+    points.push([p.x,p.y]);
+    }
+  delaunay = d3.Delaunay.from(points);
+  //-118.365114353651 14.5320983701145,-86.7104052798835 32.7186535707393
+  voronoi = delaunay.voronoi([-120,12,-80,35]);
+  polis = []
+  for ( let i of voronoi.cellPolygons()){
+    polis.push(i);
+    }
+
+  for(let [i,d] of data.entries()){
+    geoms.push(
+    {type:'Feature', properties:{CVE_ENT:d.cve_ent},
+    geometry:{type:"Polygon", coordinates:[polis[i]]}
+    }
+    );
+    }
+  
+  return {type:"FeatureCollection", features:geoms};
   }
-
-});
-
